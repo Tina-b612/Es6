90c62edbd2ee2ff55a67870fca7cf1f6e61b5e15
Tina <tina__1995@163.com>
2016-11-25 17:22:57 +0800

    Second

---
+++ module/es6/main.js
@@ -0,0 +1,2 @@
+import {name1,name2,name3,name4} from "./user.js";
+console.log(name1,name2,name3,name4)
---
+++ module/es6/user.js
@@ -0,0 +1,6 @@
+var name1 = "super-s";
+var name2 = "dazhuang";
+var name3 = "Tina";
+var name4 = "anna";
+
+export {name1,name2,name3,name4};
